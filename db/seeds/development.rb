1be0f4d960075150e5131b589ed2e408f5d0b70d
Bruno Leite <bmarianoleite3@gmail.com>
2023-04-23 15:08:12 -0300

    adding to seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -12,12 +12,33 @@ client = Client.first_or_create(first_name: 'Gisely',
                                 phone_number: 11_978_228_466,
                                 birthday: Time.zone.now - 20.years)
 
-worker = Worker.first_or_create(first_name: 'Bruno',
-                                last_name: 'Leite',
-                                job: 'Dev',
-                                phone_number: 11_978_228_466,
-                                instagram: 'brmariano13',
-                                image_url: 'https://firebasestorage.googleapis.com/v0/b/dondoca-a6bd8.appspot.com/o/profile_images%2F1628710359214.jpeg?alt=media&token=b53d24ea-e392-4536-a4dd-de1961d99a18')
+worker = Worker.find_or_create_by(first_name: 'Bruno',
+                                  last_name: 'Leite',
+                                  job: 'Dev',
+                                  phone_number: 11_978_228_466,
+                                  instagram: 'brmariano13',
+                                  image_url: 'https://firebasestorage.googleapis.com/v0/b/dondoca-a6bd8.appspot.com/o/profile_images%2F1628710359214.jpeg?alt=media&token=b53d24ea-e392-4536-a4dd-de1961d99a18')
+
+worker2 = Worker.find_or_create_by(first_name: 'Giulia',
+                                   last_name: 'Forace',
+                                   job: 'Makeup',
+                                   phone_number: 11_978_228_467,
+                                   instagram: 'brmariano13',
+                                   image_url: 'https://firebasestorage.googleapis.com/v0/b/dondoca-a6bd8.appspot.com/o/profile_images%2F1628710359214.jpeg?alt=media&token=b53d24ea-e392-4536-a4dd-de1961d99a18')
+
+worker3 = Worker.find_or_create_by(first_name: 'Juarezita',
+                                   last_name: 'Rosa',
+                                   job: 'Manicure',
+                                   phone_number: 11_978_228_468,
+                                   instagram: 'brmariano13',
+                                   image_url: 'https://firebasestorage.googleapis.com/v0/b/dondoca-a6bd8.appspot.com/o/profile_images%2F1628710359214.jpeg?alt=media&token=b53d24ea-e392-4536-a4dd-de1961d99a18')
+
+worker4 = Worker.find_or_create_by(first_name: 'Gisely',
+                                   last_name: 'Rosa',
+                                   job: 'Hairstylist',
+                                   phone_number: 11_978_228_469,
+                                   instagram: 'brmariano13',
+                                   image_url: 'https://firebasestorage.googleapis.com/v0/b/dondoca-a6bd8.appspot.com/o/profile_images%2F1628710359214.jpeg?alt=media&token=b53d24ea-e392-4536-a4dd-de1961d99a18')
 
 Schedule.create(start_at: Time.zone.now,
                 end_at: Time.zone.now + 30.minutes,
@@ -27,4 +48,29 @@ Schedule.create(start_at: Time.zone.now,
 Schedule.create(start_at: Time.zone.now + 1.hour,
                 end_at: Time.zone.now + 2.hours,
                 client:,
+                worker: worker2)
+
+Schedule.create(start_at: Time.zone.now + 1.hour,
+                end_at: Time.zone.now + 2.hours,
+                client:,
+                worker: worker3)
+
+Schedule.create(start_at: Time.zone.now + 2.hours,
+                end_at: Time.zone.now + 3.hours,
+                client:,
+                worker: worker3)
+
+Schedule.create(start_at: Time.zone.now + 1.hour,
+                end_at: Time.zone.now + 2.hours,
+                client:,
                 worker:)
+
+Schedule.create(start_at: Time.zone.now + 3.hours,
+                end_at: Time.zone.now + 3.hours,
+                client:,
+                worker:)
+
+Schedule.create(start_at: Time.zone.now + 4.hours,
+                end_at: Time.zone.now + 4.hours,
+                client:,
+                worker: worker4)
